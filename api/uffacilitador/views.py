4b1b5a5fa3bb69eafde1ca278ae08c3801b6b358
Glaucio <31291125+helsinq@users.noreply.github.com>
2019-11-11 11:40:59 -0300

    Update views

--- api/uffacilitador/views.py
+++ api/uffacilitador/views.py
@@ -1,19 +1,40 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import viewsets
-from api.uffacilitador.serializers import UserSerializer, GroupSerializer
+from rest_framework import generics
+from api.uffacilitador.models import Cursos, Usuarios, Disciplinas, GradeCurricular, Repositorios, Sistemas
+from api.uffacilitador.serializers import UsuariosSerializer, CursosSerializer, DisciplinasSerializer, GradeCurricularSerializer, RepositoriosSerializer, SistemasSerializer
 
 
+"""
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
+"""
+
+class UsuariosList(generics.ListCreateAPIView):
+    queryset = Usuarios.objects.all()
+    serializer_class = UsuariosSerializer
+
+    
+class CursosList(generics.ListCreateAPIView):
+    queryset = Cursos.objects.all()
+    serializer_class = CursosSerializer
 
 
-class GroupViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows groups to be viewed or edited.
-    """
-    queryset = Group.objects.all()
-    serializer_class = GroupSerializer
+class DisciplinasList(generics.ListCreateAPIView):
+    queryset = Disciplinas.objects.all()
+    serializer_class = DisciplinasSerializer
+
+    
+class GradeCurricularList(generics.ListCreateAPIView):
+    queryset = GradeCurricular.objects.all()
+    serializer_class = GradeCurricularSerializer
+
+
+class RepositoriosList(generics.ListCreateAPIView):
+    queryset = Repositorios.objects.all()
+    serializer_class = RepositoriosSerializer
+
+    
+class SistemasList(generics.ListCreateAPIView):
+    queryset = Sistemas.objects.all()
+    serializer_class = SistemasSerializer
+    
